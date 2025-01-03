fe3ec5350819fba797ffbbca678effd65095dc02
Kent Tuneberg <ktunebe@gmail.com>
2025-01-03 00:52:57 -0600

    separated recipe list into categories

--- src/RecipeList.jsx
+++ src/RecipeList.jsx
@@ -2,18 +2,48 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function RecipeList({ recipes }) {
+  const salads = [];
+  const appetizers = [];
+
+  recipes.forEach((recipe) => {
+    switch (recipe.category) {
+      case 'salad':
+        salads.push(recipe);
+        break;
+      case 'appetizer':
+        appetizers.push(recipe);
+        break;
+      default:
+        break;
+    }
+  });
+
   return (
-    <ul>
-      {recipes.map((recipe) => {
-        console.log('Recipe:', recipe); // Log the recipe object
-        const recipeId = recipe.id.toString(); // Ensure recipe.id is converted to a string
-        return (
-          <li key={recipeId}>
-            <Link to={`/recipes/${recipeId}`}>{recipe.title}</Link>
-          </li>
-        );
-      })}
-    </ul>
+    <div>
+      <h2>Salads</h2>
+      <ul>
+        {salads.map((recipe) => {
+          // const recipeId = recipe._id.toString(); // Ensure recipeId is correctly defined
+          return (
+            <li key={recipe.id}>
+              <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+
+      <h2>Appetizers</h2>
+      <ul>
+        {appetizers.map((recipe) => {
+          // const recipeId = recipe._id.toString(); // Ensure recipeId is correctly defined
+          return (
+            <li key={recipe.id}>
+              <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
 
