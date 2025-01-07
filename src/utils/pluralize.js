72b5603df1521d6063d9ba64d173d3c0c6dc0667
Kent Tuneberg <ktunebe@gmail.com>
2025-01-07 04:37:57 -0600

    reverted changes

--- src/utils/pluralize.js
+++ src/utils/pluralize.js
@@ -1,6 +1,6 @@
 export function pluralize(string) {
   switch (true) {
-    case string.endsWith('s' || 'h'):
+    case string.endsWith('s') || string.endsWith('h'):
       return `${string}es`;
     case ['tsp', 'Tbsp', 'oz'].includes(string):
       return string;
